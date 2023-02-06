a7a4f5d647208aea5b823957e7507a27b0e292b2
or <orgabai1212@gmail.com>
2023-02-06 19:08:49 +0200

    saving files

---
+++ app.py
@@ -0,0 +1,102 @@
+from flask import Flask,request,jsonify
+import mysql.connector
+import time
+
+
+app = Flask(__name__)
+#conet to mysql
+def connect():
+    return mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="crc",
+    port="3306"
+    )
+#a sql command function (work only to get information and not for changes in the db)
+def sql_command(command):
+    conection =connect()
+    cursor = conection.cursor()
+    cursor.execute(command)
+    result=cursor.fetchall()
+    cursor.close()
+    conection.close()
+    return result
+
+#the insert function
+def sql_insert(id,first_name,last_name):
+    conection =connect()
+    cursor = conection.cursor()
+    qury="insert into criminals (id,first_name,last_name) values (%s,%s,%s)"
+    values=(id,first_name,last_name)
+    cursor.execute(qury,values)
+    conection.commit()
+    cursor.close()
+    conection.close()
+
+def sql_delete(id):
+  conection=connect()
+  cursor = conection.cursor()
+  qury=f"delete from criminals where id = {id};"
+  cursor.execute(qury)
+  conection.commit()
+  cursor.close()
+  conection.close()
+
+
+
+@app.route('/')
+def welcom():
+  return "welcom"
+
+#insert a new criminal in db
+@app.route('/add',methods=['POST'])
+def add():
+    data = request.get_json()
+    id = data['id']
+    first_name = data['first_name']
+    last_name = data['last_name']
+    
+    sql_insert(id,first_name,last_name)
+    return "ok"
+
+#get all criminal list im db
+@app.route('/all',methods=['GET'])
+def all():
+  result =sql_command("select * from criminals;")
+  return jsonify(result)
+
+
+#serch for Specific criminal in db
+@app.route('/find',methods=['GET'])
+def find():
+  check_id= request.args.get('id')   
+  command =f"select id,first_name,last_name from criminals where id ={check_id};"
+  criminal=sql_command(command)
+  #criminal became a list 
+  if len(criminal)==0:
+    return "criminal not found"
+  else:
+    return jsonify(criminal)
+
+
+
+@app.route('/delete',methods=['GET'])
+def delete():
+  criminal_to_rm=request.args.get('id')
+  check_before=sql_command("select * from criminals;")
+  print(len(check_before))
+  sql_delete(criminal_to_rm)
+  time.sleep(5)
+  print(len(check_after))
+  check_after=sql_command("select * from criminals;")
+  if len(check_before)==len(check_after):
+    
+    return "wrong id"
+  else:
+    return "criminal removed"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
